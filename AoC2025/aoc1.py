a3d9e7c7b3c29267b238de17ee3b018546ea7b76
Chase Cassels <chasecassels@gmail.com>
2025-12-16 12:13:07 -0800

    first puzzle solution

--- AoC2025/aoc1.py
+++ AoC2025/aoc1.py
@@ -1,16 +1,21 @@
+def main():
 
+    file = 'aoc1-input.txt'
+    pos = 50 
+    ans = 0
 
-file = 'aoc1-input.txt' # Make sure this path is correct
+    with open(file, 'r') as f:
+        moves = [line.strip() for line in f]
 
-with open(file, 'r') as f:
+    for move in moves:
+        if move[0] == 'L':
+            pos = (pos - int(move[1:])) % 100
+        else:
+            pos = (pos + int(move[1:])) % 100
+        if pos == 0:
+            ans += 1
 
-    moves = [line.strip() for line in f]
-
-pos = 50 
-
-for move in moves:
-    if move[0] == 'L':
-        pos -= move[1:]
-    else:
-        pos += move[1:]
+    print(ans)
     
+if __name__ == "__main__":
+    main()
